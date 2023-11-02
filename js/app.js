e23b6e7eeddb581e6c9389fc38699542e60000da
Jabrail <138629447+JaShu01@users.noreply.github.com>
2023-11-02 17:21:21 +0100

    app.js

--- js/app.js
+++ js/app.js
@@ -34,22 +34,22 @@ document.addEventListener("DOMContentLoaded", showCoffees);
 if (navigator.serviceWorker) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-    .register("./serviceWorker.js", { scope: "./" })
-    
+      .register("/sw.js")
       .then(regEvent => console.log("Service worker registered!"))
       .catch(err => console.log("Service worker not registered"));
   });
 }
+
 function getUserMedia(constraints) {
   // if Promise-based API is available, use it
   if (navigator.mediaDevices) {
     return navigator.mediaDevices.getUserMedia(constraints);
   }
-    
+
   // otherwise try falling back to old, possibly prefixed API...
   var legacyApi = navigator.getUserMedia || navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia || navigator.msGetUserMedia;
-    
+
   if (legacyApi) {
     // ...and promisify it
     return new Promise(function (resolve, reject) {
@@ -58,7 +58,7 @@ function getUserMedia(constraints) {
   }
 }
 
-function getStream (type) {
+function getStream(type) {
   if (!navigator.mediaDevices && !navigator.getUserMedia && !navigator.webkitGetUserMedia &&
     !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
     alert('User Media API not supported.');
@@ -67,11 +67,11 @@ function getStream (type) {
 
   var constraints = {};
   constraints[type] = true;
-  
+
   getUserMedia(constraints)
     .then(function (stream) {
       var mediaControl = document.querySelector(type);
-      
+
       if ('srcObject' in mediaControl) {
         mediaControl.srcObject = stream;
       } else if (navigator.mozGetUserMedia) {
@@ -79,103 +79,19 @@ function getStream (type) {
       } else {
         mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
       }
-      
+
       mediaControl.play();
     })
     .catch(function (err) {
       alert('Error: ' + err);
     });
-   
-}
-function getUserMedia(options, successCallback, failureCallback) {
-  var api = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia || navigator.msGetUserMedia;
-  if (api) {
-    return api.bind(navigator)(options, successCallback, failureCallback);
-  }
 }
 
-var theStream;
-var theRecorder;
-var recordedChunks = [];
+// Event-Handler für die Button-Elemente
+document.querySelector('button[data-action="video"]').addEventListener('click', function () {
+  getStream('video');
+});
 
-function getStream() {
-  if (!navigator.getUserMedia && !navigator.webkitGetUserMedia &&
-    !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
-    alert('User Media API not supported.');
-    return;
-  }
-  
-  var constraints = {video: true, audio: true};
-  getUserMedia(constraints, function (stream) {
-    var mediaControl = document.querySelector('video');
-    
-    if ('srcObject' in mediaControl) {
-      mediaControl.srcObject = stream;
-    } else if (navigator.mozGetUserMedia) {
-      mediaControl.mozSrcObject = stream;
-    } else {
-      mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
-    }
-    
-    theStream = stream;
-    try {
-      recorder = new MediaRecorder(stream, {mimeType : "video/webm"});
-    } catch (e) {
-      console.error('Exception while creating MediaRecorder: ' + e);
-      return;
-    }
-    theRecorder = recorder;
-    console.log('MediaRecorder created');
-    recorder.ondataavailable = recorderOnDataAvailable;
-    recorder.start(100);
-  }, function (err) {
-    alert('Error: ' + err);
-  });
-}
-
-function recorderOnDataAvailable(event) {
-  if (event.data.size == 0) return;
-  recordedChunks.push(event.data);
-}
-
-function download() {
-  console.log('Saving data');
-  theRecorder.stop();
-  theStream.getTracks()[0].stop();
-
-  var blob = new Blob(recordedChunks, {type: "video/webm"});
-  var url = (window.URL || window.webkitURL).createObjectURL(blob);
-  var a = document.createElement("a");
-  document.body.appendChild(a);
-  a.style = "display: none";
-  a.href = url;
-  a.download = 'test.webm';
-  a.click();
-  
-  // setTimeout() here is needed for Firefox.
-  setTimeout(function () {
-      (window.URL || window.webkitURL).revokeObjectURL(url);
-  }, 100); }
-  // In deiner JavaScript-Datei (app.js)
-async function loadVideoFromCache() {
-  const videoUrl = 'Pfad_zu_deinem_Video.mp4'; // Ersetze mit dem tatsächlichen Pfad zu deinem Video
-
-  try {
-    const cache = await caches.open('video-cache');
-    const response = await cache.match(videoUrl); // Versuche, das Video aus dem Cache zu laden
-    if (response) {
-      const videoBlob = await response.blob();
-      const videoElement = document.querySelector('video');
-      videoElement.src = URL.createObjectURL(videoBlob); // Zeige das Video im <video>-Element
-      videoElement.play(); // Starte die Wiedergabe
-      console.log('Video wurde aus dem Cache abgerufen und wiedergegeben.');
-    } else {
-      console.log('Video nicht im Cache gefunden.');
-    }
-  } catch (error) {
-    console.error('Fehler beim Abrufen des Videos aus dem Cache:', error);
-  }
-}
-
-
+document.querySelector('button[data-action="audio"]').addEventListener('click', function () {
+  getStream('audio');
+});
