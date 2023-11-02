0f78eb5e645726b866086d10a1e682cd0225148c
Jabrail <138629447+JaShu01@users.noreply.github.com>
2023-11-02 18:01:54 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,36 +1,3 @@
-const container = document.querySelector(".container");
-
-const coffees = [
-  { name: "Perspiciatis", image: "images/coffee1.jpg" },
-  { name: "Voluptatem", image: "images/coffee2.jpg" },
-  { name: "Explicabo", image: "images/coffee3.jpg" },
-  { name: "Rchitecto", image: "images/coffee4.jpg" },
-  { name: " Beatae", image: "images/coffee5.jpg" },
-  { name: " Vitae", image: "images/coffee6.jpg" },
-  { name: "Inventore", image: "images/coffee7.jpg" },
-  { name: "Veritatis", image: "images/coffee8.jpg" },
-  { name: "Accusantium", image: "images/coffee9.jpg" }
-];
-
-const showCoffees = () => {
-  let output = "";
-
-  coffees.forEach(
-    ({ name, image }) =>
-      (output += `
-      <div class="card">
-        <img class="card--avatar" src=${image} />
-        <h1 class="card--title"> ${name} </h1>
-        <a class="card--link" href="#">Taste</a>
-      </div>
-    `)
-  );
-
-  container.innerHTML = output;
-};
-
-document.addEventListener("DOMContentLoaded", showCoffees);
-
 if (navigator.serviceWorker) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
@@ -41,12 +8,12 @@ if (navigator.serviceWorker) {
 }
 
 function getUserMedia(constraints) {
-  // if Promise-based API is available, use it
+  // If Promise-based API is available, use it
   if (navigator.mediaDevices) {
     return navigator.mediaDevices.getUserMedia(constraints);
   }
 
-  // otherwise try falling back to old, possibly prefixed API...
+  // Otherwise try falling back to old, possibly prefixed API...
   var legacyApi = navigator.getUserMedia || navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
@@ -95,3 +62,24 @@ document.querySelector('button[data-action="video"]').addEventListener('click', 
 document.querySelector('button[data-action="audio"]').addEventListener('click', function () {
   getStream('audio');
 });
+
+function takePhoto() {
+  if (!('ImageCapture' in window)) {
+    alert('ImageCapture is not available');
+    return;
+  }
+
+  if (!theStream) {
+    alert('Grab the video stream first!');
+    return;
+  }
+
+  var theImageCapturer = new ImageCapture(theStream.getVideoTracks()[0]);
+
+  theImageCapturer.takePhoto()
+    .then(blob => {
+      var theImageTag = document.getElementById("imageTag");
+      theImageTag.src = URL.createObjectURL(blob);
+    })
+    .catch(err => alert('Error: ' + err));
+}
