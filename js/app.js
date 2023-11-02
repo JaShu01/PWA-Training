f340769bd9bda86e5b1a93be8dd9f81bdc59a1bd
Jabrail <138629447+JaShu01@users.noreply.github.com>
2023-11-02 17:39:19 +0100

    app.js

--- js/app.js
+++ js/app.js
@@ -40,23 +40,15 @@ if (navigator.serviceWorker) {
   });
 }
 
-function getUserMedia(constraints) {
-  // if Promise-based API is available, use it
-  if (navigator.mediaDevices) {
-    return navigator.mediaDevices.getUserMedia(constraints);
-  }
-
-  // otherwise try falling back to old, possibly prefixed API...
-  var legacyApi = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+function getUserMedia(options, successCallback, failureCallback) {
+  var api = navigator.getUserMedia || navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia || navigator.msGetUserMedia;
-
-  if (legacyApi) {
-    // ...and promisify it
-    return new Promise(function (resolve, reject) {
-      legacyApi.bind(navigator)(constraints, resolve, reject);
-    });
+  if (api) {
+    return api.bind(navigator)(options, successCallback, failureCallback);
   }
 }
+
+var theStream;
 
 function getStream(type) {
   if (!navigator.mediaDevices && !navigator.getUserMedia && !navigator.webkitGetUserMedia &&
@@ -68,23 +60,22 @@ function getStream(type) {
   var constraints = {};
   constraints[type] = true;
 
-  getUserMedia(constraints)
-    .then(function (stream) {
-      var mediaControl = document.querySelector(type);
+  getUserMedia(constraints, function (stream) {
+    var mediaControl = document.querySelector(type);
 
-      if ('srcObject' in mediaControl) {
-        mediaControl.srcObject = stream;
-      } else if (navigator.mozGetUserMedia) {
-        mediaControl.mozSrcObject = stream;
-      } else {
-        mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
-      }
+    if ('srcObject' in mediaControl) {
+      mediaControl.srcObject = stream;
+    } else if (navigator.mozGetUserMedia) {
+      mediaControl.mozSrcObject = stream;
+    } else {
+      mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
+    }
 
-      mediaControl.play();
-    })
-    .catch(function (err) {
-      alert('Error: ' + err);
-    });
+    mediaControl.play();
+    theStream = stream;
+  }, function (err) {
+    alert('Error: ' + err);
+  });
 }
 
 // Event-Handler für die Button-Elemente
@@ -95,5 +86,27 @@ document.querySelector('button[data-action="video"]').addEventListener('click', 
 document.querySelector('button[data-action="audio"]').addEventListener('click', function () {
   getStream('audio');
 });
+
+function takePhoto() {
+  if (!('ImageCapture' in window)) {
+    alert('ImageCapture is not available');
+    return;
+  }
+
+  if (!theStream) {
+    alert('Grab the video stream first!');
+    return;
+  }
+
+  var theImageCapturer = new ImageCapture(theStream.getVideoTracks()[0]);
+
+  theImageCapturer.takePhoto()
+    .then(blob => {
+      var theImageTag = document.getElementById("imageTag");
+      theImageTag.src = URL.createObjectURL(blob);
+    })
+    .catch(err => alert('Error: ' + err));
+}
+
 
 
