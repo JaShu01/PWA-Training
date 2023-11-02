397423ae96f902ee967d1308cfcc974d590feefe
Jabrail <138629447+JaShu01@users.noreply.github.com>
2023-11-02 17:41:56 +0100

    app.js

--- js/app.js
+++ js/app.js
@@ -1,54 +1,27 @@
 const container = document.querySelector(".container");
 
-const coffees = [
-  { name: "Perspiciatis", image: "images/coffee1.jpg" },
-  { name: "Voluptatem", image: "images/coffee2.jpg" },
-  { name: "Explicabo", image: "images/coffee3.jpg" },
-  { name: "Rchitecto", image: "images/coffee4.jpg" },
-  { name: " Beatae", image: "images/coffee5.jpg" },
-  { name: " Vitae", image: "images/coffee6.jpg" },
-  { name: "Inventore", image: "images/coffee7.jpg" },
-  { name: "Veritatis", image: "images/coffee8.jpg" },
-  { name: "Accusantium", image: "images/coffee9.jpg" }
-];
 
-const showCoffees = () => {
-  let output = "";
-
-  coffees.forEach(
-    ({ name, image }) =>
-      (output += `
-      <div class="card">
-        <img class="card--avatar" src=${image} />
-        <h1 class="card--title"> ${name} </h1>
-        <a class="card--link" href="#">Taste</a>
-      </div>
-    `)
-  );
-
-  container.innerHTML = output;
-};
 
 document.addEventListener("DOMContentLoaded", showCoffees);
 
-if (navigator.serviceWorker) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then(regEvent => console.log("Service worker registered!"))
-      .catch(err => console.log("Service worker not registered"));
-  });
-}
 
-function getUserMedia(options, successCallback, failureCallback) {
-  var api = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+function getUserMedia(constraints) {
+  // if Promise-based API is available, use it
+  if (navigator.mediaDevices) {
+    return navigator.mediaDevices.getUserMedia(constraints);
+  }
+
+  // otherwise try falling back to old, possibly prefixed API...
+  var legacyApi = navigator.getUserMedia || navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia || navigator.msGetUserMedia;
-  if (api) {
-    return api.bind(navigator)(options, successCallback, failureCallback);
+
+  if (legacyApi) {
+    // ...and promisify it
+    return new Promise(function (resolve, reject) {
+      legacyApi.bind(navigator)(constraints, resolve, reject);
+    });
   }
 }
-
-var theStream;
 
 function getStream(type) {
   if (!navigator.mediaDevices && !navigator.getUserMedia && !navigator.webkitGetUserMedia &&
@@ -60,22 +33,23 @@ function getStream(type) {
   var constraints = {};
   constraints[type] = true;
 
-  getUserMedia(constraints, function (stream) {
-    var mediaControl = document.querySelector(type);
+  getUserMedia(constraints)
+    .then(function (stream) {
+      var mediaControl = document.querySelector(type);
 
-    if ('srcObject' in mediaControl) {
-      mediaControl.srcObject = stream;
-    } else if (navigator.mozGetUserMedia) {
-      mediaControl.mozSrcObject = stream;
-    } else {
-      mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
-    }
+      if ('srcObject' in mediaControl) {
+        mediaControl.srcObject = stream;
+      } else if (navigator.mozGetUserMedia) {
+        mediaControl.mozSrcObject = stream;
+      } else {
+        mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
+      }
 
-    mediaControl.play();
-    theStream = stream;
-  }, function (err) {
-    alert('Error: ' + err);
-  });
+      mediaControl.play();
+    })
+    .catch(function (err) {
+      alert('Error: ' + err);
+    });
 }
 
 // Event-Handler für die Button-Elemente
@@ -87,16 +61,6 @@ document.querySelector('button[data-action="audio"]').addEventListener('click', 
   getStream('audio');
 });
 
-function takePhoto() {
-  if (!('ImageCapture' in window)) {
-    alert('ImageCapture is not available');
-    return;
-  }
-
-  if (!theStream) {
-    alert('Grab the video stream first!');
-    return;
-  }
 
   var theImageCapturer = new ImageCapture(theStream.getVideoTracks()[0]);
 
