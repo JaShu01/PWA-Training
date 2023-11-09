820ae645d8c146191e44ef545281a4fbfde61e0f
Jabrail <138629447+JaShu01@users.noreply.github.com>
2023-11-09 15:14:19 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -187,3 +187,22 @@ function saveToCache(blob) {
     console.error('Cache API not supported');
   }
 }
+var target = document.getElementById('target');
+var watchId;
+
+function appendLocation(location, verb) {
+  verb = verb || 'updated';
+  var newLocation = document.createElement('p');
+  newLocation.innerHTML = 'Location ' + verb + ': ' + location.coords.latitude + ', ' + location.coords.longitude + '';
+  target.appendChild(newLocation);
+}
+
+if ('geolocation' in navigator) {
+  document.getElementById('askButton').addEventListener('click', function () {
+    navigator.geolocation.getCurrentPosition(function (location) {
+      appendLocation(location, 'fetched');
+    });
+    watchId = navigator.geolocation.watchPosition(appendLocation);
+  });
+  } else {
+      target.innerText = 'Geolocation API not supported.';
