7c442326463e9e959f5f88188ad6a334139a1c94
Jabrail <138629447+JaShu01@users.noreply.github.com>
2023-11-30 16:19:32 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -447,3 +447,47 @@ if ('getBattery' in navigator || ('battery' in navigator && 'Promise' in window)
     battery.addEventListener('levelchange', onLevelChange);
   });
 }
+document.getElementById('cost-entry-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  // Daten aus dem Formular holen
+  var description = document.getElementById('description').value;
+  var date = document.getElementById('date').value;
+  var category = document.getElementById('category').value;
+  var amount = document.getElementById('amount').value;
+
+  // Eintrag als Objekt erstellen
+  var entry = {
+    description: description,
+    date: date,
+    category: category,
+    amount: amount,
+    id: Date.now() // Einzigartige ID basierend auf der aktuellen Zeit
+  };
+
+  // Daten im LocalStorage speichern
+  var entries = JSON.parse(localStorage.getItem('entries') || '[]');
+  entries.push(entry);
+  localStorage.setItem('entries', JSON.stringify(entries));
+
+  // Formular zurücksetzen
+  document.getElementById('cost-entry-form').reset();
+});
+function displayEntries() {
+  var entries = JSON.parse(localStorage.getItem('entries') || '[]');
+  var tableBody = document.getElementById('entries-table-body');
+
+  // Tabelle leeren
+  tableBody.innerHTML = '';
+
+  // Einträge in die Tabelle einfügen
+  entries.forEach(function(entry) {
+    var row = tableBody.insertRow();
+    row.insertCell(0).textContent = entry.description;
+    row.insertCell(1).textContent = entry.date;
+    row.insertCell(2).textContent = entry.category;
+    row.insertCell(3).textContent = entry.amount;
+  });
+}
+
+
