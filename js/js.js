51a49a39c7571922fddb9979451f5406c45c0b01
Jabrail <138629447+JaShu01@users.noreply.github.com>
2023-11-30 16:41:14 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -52,3 +52,15 @@ function displayEntries() {
     row.insertCell(3).textContent = entry.amount;
   });
 }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+      // Registrierung erfolgreich
+      console.log('Service Worker Registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // Registrierung fehlgeschlagen :(
+      console.log('Service Worker Registration failed: ', err);
+    });
+  });
+}
+
